89e1b92917939716eee11f82e61c8f0d2a7449a3
JacekKajdan <47155575+JacekKajdan@users.noreply.github.com>
2024-06-20 11:09:51 +0200

    Add files via upload

--- src/components/PicList.vue
+++ src/components/PicList.vue
@@ -16,7 +16,7 @@
                 <v-card-text>
                     <div v-if="error_on_req" class="bg-red">ERROR</div>
                     <svg v-else-if="!loading" :width="500" :height="500">
-                        <rect v-for="r in svgData" :x="r.x" :y="r.y" :width="r.w" :height="r.h" :fill="r.color" />
+                        <rect v-for="r in curPic" :x="r.x" :y="r.y" :width="r.w" :height="r.h" :fill="r.color" />
                     </svg>
                     <v-progress-circular v-else indeterminate color="primary"></v-progress-circular>
                 </v-card-text>
@@ -25,10 +25,10 @@
                 </v-card-actions>
             </v-card>
         </v-dialog>
-        <div v-if="wsData>nr_old_pics">
+        <div v-if="nr_new_pics>nr_old_pics">
             <v-chip color="primary" class="ma-2">
-            Nowe obrazki!  ({{ (wsData-nr_old_pics)/10 }})</v-chip>
-            <v-btn color="primary" class="ma-2" @click="sendRequest">Pobierz nowe obrazki</v-btn>
+            Nowe obrazki!  ({{ (nr_new_pics-nr_old_pics)/10 }})</v-chip>
+            <v-btn color="primary" class="ma-2" @click="get_all_pic">Pobierz nowe obrazki</v-btn>
 
         </div>
         
@@ -45,46 +45,52 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            items: [],
+            pics: [],
             currentPage: 1,
-            pageSize: 3, // Number of items per page
+            pageSize: 3,
             totalPages: 1,
             dialogVisible: false,
-            selectedItem: -1,
-            svgData: null,
+            curPic: null,
             loading: false,
             error_on_req: false,
             ws: null,
-            wsData: 0,
+            nr_new_pics: 0,
             nr_old_pics: 0
         };
     },
+    //Przy uruchomieniu
     mounted() {
-        this.sendRequest();
+        this.get_all_pic();
         this.setupWebSocket();
     },
+
+    //Koniec polaczenia z ws
     beforeDestroy() {
         if (this.ws) {
             this.ws.close();
         }
     },
+
+    //Obrazki na danej stronie
     computed: {
         displayedItems() {
             const startIndex = (this.currentPage - 1) * this.pageSize;
-            return this.items.slice(startIndex, startIndex + this.pageSize);
+            return this.pics.slice(startIndex, startIndex + this.pageSize);
         }
     },
+
+
     methods: {
         openDialog(index) {
-            this.selectedItem = index;
             this.loading = true
-            this.fetchSVGData(index);
+            this.get_pic(index);
             this.dialogVisible = true;
 
         },
-        async fetchSVGData(index) {
+
+        //Pobranie jednego obrazka z serwera
+        async get_pic(index) {
             try {
-                // Example: Sending a POST request to FastAPI with requestData
                 const response = await axios.get('http://localhost:8000/pics/' + index.toString(),
                     {
                         method: "GET",
@@ -94,19 +100,24 @@ export default {
                             "Accept": "application/json",
                         },
                     });
-                console.log('Request sent successfully:', response.data);
-                this.svgData = response.data;
+
+                console.log('Pobrano:', response.data);
+                this.curPic = response.data;
                 this.loading = false
-                if (this.svgData.length > 1000) {
-                    this.svgData = null
+
+                //Bledne pobranie
+                if (this.curPic.length > 1000) {
+                    this.curPic = null
                     this.error_on_req = true
                 }
             } catch (error) {
                 this.error_on_req = true
-                console.error('Error sending request:', error);
+                console.error('Blad podczas pobierania obrazka:', error);
             }
         },
-        async sendRequest() {
+
+        //Pobranie wszystkich obrazkow
+        async get_all_pic() {
             try {
                 const response = await axios.get('http://127.0.0.1:8000/get_all_pics/',
                     {
@@ -118,12 +129,14 @@ export default {
                         },
                     }
                 );
-                this.items = response.data;
-                this.totalPages = Math.ceil(this.items.length / this.pageSize);
-                this.nr_old_pics=this.wsData
+
+                console.log('Pobrano:', response.data);
+                this.pics = response.data;
+                this.totalPages = Math.ceil(this.pics.length / this.pageSize);
+                this.nr_old_pics=this.nr_new_pics
                 
             } catch (error) {
-                console.error('Error fetching items:', error);
+                console.error('Blad podczas pobierania wszystkich obrazkow:', error);
             }
         },
         closeDialog() {
@@ -137,29 +150,24 @@ export default {
         setupWebSocket() {
         this.ws = new WebSocket('ws://localhost:8000/ws2');
         this.ws.onmessage = (event) => {
-            this.wsData = event.data; 
-            this.wsData = JSON.parse(this.wsData)+1
+            this.nr_new_pics = event.data; 
+            this.nr_new_pics = JSON.parse(this.nr_new_pics)+1
+
             if(this.nr_old_pics==0){
-                this.nr_old_pics=this.wsData
+                this.nr_old_pics=this.nr_new_pics
             }
-            console.log(this.wsData)
-            console.log(this.nr_old_pics)
             
         };
         this.ws.onopen = () => {
-            console.log('WebSocket connection opened');
+            console.log('WebSocket otwarty');
         };
         this.ws.onclose = () => {
-            console.log('WebSocket connection closed');
+            console.log('WebSocket zamkniety');
         };
         this.ws.onerror = (error) => {
-            console.error('WebSocket error:', error);
+            console.error('Blad przy WebSocket:', error);
         };
         }
     }
 };
 </script>
-
-<style>
-/* Add your component's styles here */
-</style>
